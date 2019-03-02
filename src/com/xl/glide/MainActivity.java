c42ff2cb99192f8f89a888f9ec93e23a57066d45
2541012655@qq.com <2541012655@qq.com>
2019-03-02 21:45:50 +0800

    。。。

--- src/com/xl/glide/MainActivity.java
+++ src/com/xl/glide/MainActivity.java
@@ -4,14 +4,24 @@ import android.app.*;
 import android.os.*;
 import android.view.*;
 import android.widget.*;
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends Activity
 {
+	ImageView imageView ;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
 	{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+		
+		
+		String url = "http://www.yzjlb.net/app/opmrcc/ic_launcher.png";
+		ImageView imageView = (ImageView) findViewById(R.id.ImageView1);
+		Glide.with(this)
+			.load(url)
+			.into(imageView);
+		
     }
 }
